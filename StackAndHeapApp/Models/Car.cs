e49df65745aac165f97857a02692f6d6143325a6
Stanislaw Makievsky <burnfeniks@yandex.ru>
2023-02-03 14:00:10 +0300

    first commit

---
+++ StackAndHeapApp/Models/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndHeapApp.Models
+{
+    public class Car
+    {
+        public double Fuel { get; set; }
+        
+        public Car CreateCar(double fuel)
+        {
+            Car car = new Car(); // ссылка на класс Car
+            car.Fuel = fuel; // Обращаемся к полю класса
+            return car;
+        }
+}
+}
---
+++ StackAndHeapApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using StackAndHeapApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndHeapApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            
+
+            Console.WriteLine(pr.ConvertDataToDegree(8));
+            Car car1 = new Car();
+            Console.WriteLine(car1.Fuel);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// STACK
+        /// </summary>
+        /// <param name="degree"></param>
+        /// <returns></returns>
+        public double ConvertDataToDegree(int degree)
+        {
+            Car car = new Car();
+            car.Fuel = 10;
+            Console.WriteLine(car.Fuel);
+
+            return Math.Pow(2, degree);
+        }
+
+
+
+        
+    }
+}
